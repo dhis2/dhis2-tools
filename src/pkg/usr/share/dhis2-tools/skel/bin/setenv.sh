2d039d0f8d3859f8a107ce6673dcd2f2ca61b0a4
Bob Jolliffe <bobjolliffe@gmail.com>
2018-08-15 11:40:23 +0100

    Small fix to take into account tomcat 8 instead of 7

--- src/pkg/usr/share/dhis2-tools/skel/bin/setenv.sh
+++ src/pkg/usr/share/dhis2-tools/skel/bin/setenv.sh
@@ -18,7 +18,7 @@ HEAP=2G
 # venture below here only if you know what you are doing ....
 
 # sets basic memory size parameters
-export CATALINA_OPTS="-Xms$HEAP -Xmx$HEAP -XX:MaxPermSize=256m -Xss256m"
+export CATALINA_OPTS="-Xms$HEAP -Xmx$HEAP"
 
 # some best practice suggestions from https://gist.github.com/terrancesnyder/986029
 export CATALINA_OPTS="$CATALINA_OPTS -XX:+UseParallelGC"
@@ -28,5 +28,5 @@ export CATALINA_OPTS="$CATALINA_OPTS -server"
 
 # You can generally leave these untouched
 export CATALINA_PID=$CATALINA_BASE/tomcat.pid
-export CATALINA_HOME=/usr/share/tomcat7
+export CATALINA_HOME=/usr/share/tomcat8
 export DHIS2_HOME=$CATALINA_BASE
