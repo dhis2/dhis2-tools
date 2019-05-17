07487979cf528d030a736c69da15c94f4df07930
Bob Jolliffe <bobjolliffe@gmail.com>
2019-05-17 10:54:40 +0100

    Removed dependency on Oracle jvm

--- install.sh
+++ install.sh
@@ -25,14 +25,11 @@ fi
 # Only tested on LTS Ubuntu versions
 case $RELEASE in
 
-  14.04)
-    echo "installing on 14.04";;
-
-  16.04)
-    echo "installing on 16.04";;
+  18.04)
+    echo "installing on 18.04 (bionic)";;
 
   *)
-    echo "The PPA only contains packages for Ubuntu LTS 14.04 and 16.04";
+    echo "The PPA only contains packages for Ubuntu LTS 18.04";
     echo "You can build for other versions if you want, the source code is located at https://github.com/dhis2/dhis2-tools";
     echo "Exiting ...";
     exit 1;;
@@ -42,21 +39,17 @@ esac
 #add PPA
 apt-get install software-properties-common
 add-apt-repository -y ppa:bobjolliffe/dhis2-tools
-add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
 
-#accept oracle license
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-
 #install java8 and dhis2-tools
-apt-get -y install oracle-java8-installer
+apt-get -y install openjdk-8-jre-headless
 apt-get -y install dhis2-tools
 
 
 # Uncomment below to install postgres and nginx servers on this machine
 # apt-get -y install nginx postgresql
 echo "The dhis2-tools are now installed. You may also want to"
-echo "install nginx and postgresql servers on this machine. You"
+echo "install nginx (or apache2) and postgresql servers on this machine. You"
 echo "can do so by running:"
 echo
 echo "apt-get install nginx postgresql"
